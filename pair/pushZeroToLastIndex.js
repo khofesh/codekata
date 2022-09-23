4df5a60d27f1de483c442bf8661d3ad97f03b132
fahmi ahmad <fahmiahmad@tutanota.com>
2022-09-23 13:52:49 +0700

    update pair

--- pair/pushZeroToLastIndex.js
+++ pair/pushZeroToLastIndex.js
@@ -1,13 +1,25 @@
+function pushAllZeroToLastIndex(num) {
+  let nonZeros = [];
+  let zeros = [];
 
-const num=[1,2,5,6,7,0,0,54]
-function pushAllZeroToLastIndex (num){
-    let nonZeros=[]
-    let zeros=[]
-    const num = num.length
-    for(let i=0;i<num;i++){
-        
+  const len = num.length;
+
+  for (let index = 0; index < num.length; index++) {
+    // const element = num[index];
+    if (num[index] === 0) {
+      zeros.push(num[index]);
+    } else {
+      nonZeros.push(num[index]);
     }
+  }
 
+  return [...nonZeros, ...zeros];
 }
 
-console.log("result==",pushAllZeroToLastIndex(num) )
+(() => {
+  const num = [1, 2, 5, 6, 7, 0, 0, 54];
+  const num2 = [1, 2, 0, 0, 0, 3, 6];
+
+  console.log("result1: ", pushAllZeroToLastIndex(num));
+  console.log("result2: ", pushAllZeroToLastIndex(num2));
+})();
