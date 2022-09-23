c1a4d0f233daff6c57676e1decb888e9591c9164
Sikeang <sikeanag@slash.co>
2022-09-23 13:46:55 +0700

    init function

---
+++ pair/pushZeroToLastIndex.js
@@ -0,0 +1,13 @@
+
+const num=[1,2,5,6,7,0,0,54]
+function pushAllZeroToLastIndex (num){
+    let nonZeros=[]
+    let zeros=[]
+    const num = num.length
+    for(let i=0;i<num;i++){
+        
+    }
+
+}
+
+console.log("result==",pushAllZeroToLastIndex(num) )
